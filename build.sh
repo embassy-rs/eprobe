100a8d6fa3f983b9c0955ac838005b1b7c2079a0
Dario Nieuwenhuis <dirbaio@dirbaio.net>
2025-07-02 23:33:32 +0200

    Update build.sh.

--- build.sh
+++ build.sh
@@ -4,10 +4,23 @@ set -euxo pipefail
 
 cd $(dirname $0)
 
+rm -rf build
+mkdir -p build
+
+# bootloader
 (cd bootloader; cargo build --release)
+cp bootloader/target/thumbv7m-none-eabi/release/eprobe-bootloader build/bootloader.elf
+llvm-objcopy -O binary build/bootloader.elf build/bootloader.bin
+
+# bootloader loader
+# it embeds the bootloader, so must be built after.
+(cd bootloaderloader; cargo build --release)
+cp bootloaderloader/target/thumbv7m-none-eabi/release/eprobe-bootloaderloader build/bootloaderloader.elf
+llvm-objcopy -O binary build/bootloaderloader.elf build/bootloaderloader.bin
+
+# application
 (cd application; cargo build --release)
-cp bootloader/target/thumbv7m-none-eabi/release/eprobe-bootloader flasher/bootloader.elf
-cp application/target/thumbv7m-none-eabi/release/eprobe flasher/application.elf
-llvm-objcopy -O binary flasher/application.elf flasher/application.bin
+cp application/target/thumbv7m-none-eabi/release/eprobe build/application.elf
+llvm-objcopy -O binary build/application.elf build/application.bin
 
 #sudo dfu-util -d c0de:cafe,c0de:caff -D application.bin -R
